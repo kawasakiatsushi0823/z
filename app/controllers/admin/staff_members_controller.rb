30e4d276cb52265e8b5fed0a80ab4471459954d5
kawasakiatsushi <kawasaki8910@gmail.com>
2014-09-17 02:47:30 +0900

    admin職員管理画面作成

--- app/controllers/admin/staff_members_controller.rb
+++ app/controllers/admin/staff_members_controller.rb
@@ -11,4 +11,48 @@ class Admin::StaffMembersController < Admin::Base
   def new
     @staff_member = StaffMember.new
   end
+
+  def edit
+    @staff_member = StaffMember.find(params[:id])
+  end
+
+  def create
+    @staff_member = StaffMember.new(staff_member_params)
+    if @staff_member.save
+      flash.notice = '職員アカウントを新規登録しました。'
+      redirect_to :admin_staff_members
+    else
+      render action: 'new'
+    end
+  end
+
+  def update
+    @staff_member = StaffMember.find(params[:id])
+    @staff_member.assign_attributes(staff_member_params)
+    if @staff_member.save
+      flash.notice = "職員アカウントを更新しました。"
+      redirect_to :admin_staff_members
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    staff_member = StaffMember.find(params[:id])
+    staff_member.destroy!
+    flash.notice = '職員アカウントを削除しました'
+    redirect_to :admin_staff_members
+  end
+
+  private
+  def staff_member_params
+    params.require(:staff_member).permit(
+      :email, :password, :family_name, :given_name,
+      :family_name_kana, :given_name_kana,
+      :start_data, :end_data, :suspended
+    )
+  end
+
 end
+
+
--- app/controllers/concerns/error_handlers.rb
+++ app/controllers/concerns/error_handlers.rb
@@ -3,8 +3,9 @@ module ErrorHandlers
 
   included do
   rescue_from Exception, with: :rescue500
-  rescue_from Forbidden, with: :rescue403
-  rescue_from IpAddressRejected, with: :rescue403
+  rescue_from ActionController::ParameterMissing, with: :rescue400
+  rescue_from ApplicationController::Forbidden, with: :rescue403
+  rescue_from ApplicationController::IpAddressRejected, with: :rescue403
   rescue_from ActionController::RoutingError, with: :rescue404
   rescue_from ActiveRecord::RecordNotFound, with: :rescue404
   end
@@ -23,6 +24,11 @@ module ErrorHandlers
   def rescue500(e)
     @exception = e
     render 'errors/internal_server_error', status: 500
+  end
+
+  def rescue400(e)
+    @exception = e
+    render 'errors/bad_request', status: 400
   end
 
 
